81a3b81e115b6f0a04431a3553a16e495fc55205
Vadim <yrl.nikulin.v@gmail>
2023-03-05 11:53:15 +0300

    длинна массива

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+string[] ModArray(string[] arr)
+{
+    int k = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            k++;
+        }
+    }
